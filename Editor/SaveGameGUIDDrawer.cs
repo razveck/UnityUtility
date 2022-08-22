d7c3348eb6258c29955c8c3b57233349d7e613fb
João Azuaga <joao.azuaga@gmail.com>
2022-08-22 16:42:32 +0200

    Added savegameGUID

---
+++ Editor/SaveGameGUIDDrawer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace razveck.UnityUtility.Editor {
+	[CustomPropertyDrawer(typeof(SaveGameGUID))]
+	public class SaveGameGUIDDrawer : PropertyDrawer{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			var guidProp = property.FindPropertyRelative("_guid");
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.LabelField("GUID", guidProp.stringValue);
+			if(GUILayout.Button("New", GUILayout.ExpandWidth(false))){
+				guidProp.stringValue = System.Guid.NewGuid().ToString();
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+}
---
+++ SaveGameGUID.cs
@@ -0,0 +1,28 @@
+﻿//Author: João Azuaga
+
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace razveck.UnityUtility {
+	[Serializable]
+	public struct SaveGameGUID {
+		[SerializeField]
+		[InlineButton(nameof(NewGUID), "New")]
+		[ReadOnly]
+		private string _guid;
+		public string Guid => _guid;
+
+		public void NewGUID() {
+			_guid = System.Guid.NewGuid().ToString();
+		}
+
+		public override string ToString() {
+			return _guid;
+		}
+
+		public static implicit operator string(SaveGameGUID id){
+			return id._guid;
+		}
+	}
+}
