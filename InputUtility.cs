4da4a667f1ce90191f6c975a8b32f0513a744f7d
João Azuaga <joao.azuaga@gmail.com>
2022-12-08 13:32:16 +0000

    Added Input and Math

---
+++ InputUtility.cs
@@ -0,0 +1,22 @@
+﻿//Author: João Azuaga
+
+using UnityEngine.EventSystems;
+
+namespace razveck.UnityUtility {
+	public static class InputUtility {
+
+		public static bool IsOverUI() {
+			bool isOverUI = false;
+#if UNITY_EDITOR
+			isOverUI = EventSystem.current.IsPointerOverGameObject();
+#elif UNITY_IOS || UNITY_ANDROID
+			isOverUI = Input.touchCount > 0 && EventSystem.current.IsPointerOverGameObject(Input.touches[0].fingerId);
+#else
+			isOverUI = EventSystem.current.IsPointerOverGameObject();
+#endif
+
+			return isOverUI;
+		}
+
+	}
+}
---
+++ MathUtility.cs
@@ -0,0 +1,13 @@
+﻿//Author: João Azuaga
+
+using UnityEngine;
+
+namespace razveck.UnityUtility {
+	public static class MathUtility{
+
+		public static float Remap(float value, float fromA, float fromB, float toA, float toB) {
+			return Mathf.Lerp(toA, toB, Mathf.InverseLerp(fromA, fromB, value));
+		}
+
+	}
+}
