762a2e57d17ef3c4ed1cbc6aaf551c1cd33144e9
razveck <razveck@gmail.com>
2023-12-21 20:54:30 +0100

    Changed Unity lifetime messages in SingletonBehaviour to virtual

--- SingletonBehaviour.cs
+++ SingletonBehaviour.cs
@@ -3,13 +3,13 @@
 using UnityEngine;
 
 namespace razveck.UnityUtility {
-	[DefaultExecutionOrder (-1000)]
+	[DefaultExecutionOrder(-1000)]
 	public abstract class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T> {
 
 		public static T Instance;
 
 		private void Awake() {
-			if(Instance != null){
+			if(Instance != null) {
 				gameObject.SafeDestroy();
 				return;
 			}
@@ -21,36 +21,36 @@ namespace razveck.UnityUtility {
 			AwakeInternal();
 		}
 
-		protected abstract void AwakeInternal();
+		protected virtual void AwakeInternal() { }
 
 		protected void OnEnable() {
-			if(Instance != this){
+			if(Instance != this) {
 				return;
 			}
 
 			OnEnableInternal();
 		}
 
-		protected abstract void OnEnableInternal();
+		protected virtual void OnEnableInternal() { }
 
 		private void Start() {
-			if(Instance != this){
+			if(Instance != this) {
 				return;
 			}
 
 			StartInternal();
 		}
 
-		protected abstract void StartInternal();
+		protected virtual void StartInternal() { }
 
-		private void OnDestroy(){
-			if(Instance != this){
+		private void OnDestroy() {
+			if(Instance != this) {
 				return;
 			}
 
 			OnDestroyInternal();
 		}
 
-		protected abstract void OnDestroyInternal();
+		protected virtual void OnDestroyInternal() { }
 	}
 }
