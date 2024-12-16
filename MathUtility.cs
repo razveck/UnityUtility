abff99d84d87bf2c1104c1205c5854497d912800
razveck <razveck@gmail.com>
2024-12-16 22:38:46 +0100

    Added LerpSmooth

--- MathUtility.cs
+++ MathUtility.cs
@@ -1,13 +1,17 @@
 ﻿//Author: João Azuaga
 
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace razveck.UnityUtility {
-	public static class MathUtility{
+	public static class MathUtility {
 
 		public static float Remap(float value, float fromA, float fromB, float toA, float toB) {
 			return Mathf.Lerp(toA, toB, Mathf.InverseLerp(fromA, fromB, value));
 		}
 
+		public static float LerpSmooth(float a, float b, float slope, float deltaTime) {
+			return b + (a - b) * math.exp(-slope * deltaTime);
+		}
 	}
 }
