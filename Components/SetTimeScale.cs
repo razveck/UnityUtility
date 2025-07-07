84b8611cac9dddb8f274760492d1295e4b85e6b3
razveck <razveck@gmail.com>
2025-07-07 15:48:39 +0200

    Added a SetTimeScale components

---
+++ Components/SetTimeScale.cs
@@ -0,0 +1,23 @@
+﻿//Author: João Azuaga
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace razveck.UnityUtility {
+	public class SetTimeScale : MonoBehaviour {
+
+		public float TimeScale = 1f;
+
+		// Use this for initialization
+		private void Start() {
+			Time.timeScale = TimeScale;
+		}
+	}
+}
